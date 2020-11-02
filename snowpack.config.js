7aebe3ae6c3b6c3abc1e568abdbc7cbed8a4c993
Joseph Allain <36985411+Gazzia29@users.noreply.github.com>
2020-11-02 16:27:38 +0100

    updated build

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,28 +1,29 @@
 module.exports = {
-  mount: {
-    public: '/',
-    src: '/_dist_',
-  },
-  plugins: [
-    '@snowpack/plugin-svelte',
-    '@snowpack/plugin-dotenv',
-  ],
-  install: [
-    /* ... */
-  ],
-  installOptions: {
-    /* ... */
-  },
-  devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    /* ... */
-  },
-  proxy: {
-    /* ... */
-  },
-  alias: {
-    /* ... */
-  },
+	mount: {
+		public: "/",
+		src: "/_dist_",
+	},
+	plugins: [
+		"@snowpack/plugin-svelte",
+		"@snowpack/plugin-dotenv",
+		["@snowpack/plugin-webpack", {}],
+	],
+	install: [
+		/* ... */
+	],
+	installOptions: {
+		/* ... */
+	},
+	devOptions: {
+		/* ... */
+	},
+	buildOptions: {
+		/* ... */
+	},
+	proxy: {
+		/* ... */
+	},
+	alias: {
+		/* ... */
+	},
 };
