38eef4e3a48a0d02fb5089ac1b173052e2069026
Joseph Allain <36985411+Gazzia29@users.noreply.github.com>
2020-11-04 08:51:34 +0100

    Added ugly button

--- src/modules/Header.svelte
+++ src/modules/Header.svelte
@@ -16,6 +16,18 @@
 			0 12.1px 47.6px rgba(0, 0, 0, 0.022), 0 29px 114px rgba(0, 0, 0, 0.03);
 		padding: 20px;
 		box-sizing: border-box;
+		flex-direction: column;
+	}
+	.info {
+		display: flex;
+		margin-bottom: 20px;
+	}
+	.button {
+		color: #6254bd;
+		padding: 10px 15px;
+		box-shadow: 0 0.4px 1.2px rgba(0, 0, 0, 0.024), 0 1.1px 3.3px rgba(0, 0, 0, 0.035),
+			0 2.7px 7.8px rgba(0, 0, 0, 0.046), 0 9px 26px rgba(0, 0, 0, 0.07);
+		border-radius: 6px;
 	}
 	#avatar {
 		flex: 0 0 auto;
@@ -54,9 +66,12 @@
 </style>
 
 <header in:fade>
-	<div id="avatar"><img src="assets/avatar.webp" alt="" /></div>
-	<div id="title">
-		<h1>Joseph Allain</h1>
-		<h2>Développeur Web Full Stack JS</h2>
+	<div class="info">
+		<div id="avatar"><img src="assets/avatar.webp" alt="" /></div>
+		<div id="title">
+			<h1>Joseph Allain</h1>
+			<h2>Développeur Web Full Stack JS</h2>
+		</div>
 	</div>
+	<div class="button">Me contacter</div>
 </header>
