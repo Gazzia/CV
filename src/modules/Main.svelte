1db03d7bb64f0c5fcc70cac716db6f64daaa3342
Joseph Allain <36985411+Gazzia29@users.noreply.github.com>
2021-01-15 08:30:19 +0100

    Added opquast

--- src/modules/Main.svelte
+++ src/modules/Main.svelte
@@ -1,6 +1,6 @@
 <script>
-	import { openTab } from "../stores.js";
-	import { fly, fade } from "svelte/transition";
+	import {openTab} from "../stores.js";
+	import {fly, fade} from "svelte/transition";
 	import Map from "./Map.svelte";
 	import Box from "./Box.svelte";
 	import Item from "./Item.svelte";
@@ -8,8 +8,112 @@
 	import LI from "./Item_LI.svelte";
 	import Tab from "./Map.svelte";
 	export let ready = false;
-	import { scrollToTop } from "svelte-scrollto";
+	import {scrollToTop} from "svelte-scrollto";
 </script>
+
+<main in:fade>
+	<img src="assets/blob1.svg" alt="" class="blob1" />
+	<img src="assets/blob2.svg" alt="" class="blob2" />
+	{#if $openTab == "infos"}
+		<div class="tab" in:fly|local={{x: -500, delay: 300}} out:fly|local={{x: -500}}>
+			<Box title="Dev skills">
+				<ItemList title="Web">
+					<LI text="HTML5, CSS/SASS responsive" rating="5" />
+					<LI text="Javascript, Typescript" rating="5" />
+					<LI text="Svelte.js" rating="5" />
+					<LI text="Google Firebase" rating="4" />
+					<LI text="Vue.js" rating="3" />
+					<LI text="Angular" rating="2" />
+					<LI text="React" rating="1" />
+				</ItemList>
+				<ItemList title="Node">
+					<LI text="Module Bundlers (Rollup, Snowpack..)" rating="3" />
+					<LI text="Electron" rating="3" />
+				</ItemList>
+				<ItemList title="Java">
+					<LI text="Maven, Spring, Hibernate" rating="1" />
+					<LI text="JDK 1.8" rating="1" />
+				</ItemList>
+				<Item solo text="MySQL" />
+			</Box>
+			<Box title="Skills">
+				<ItemList title="Soft skills">
+					<LI text="Pédagogie et écoute" />
+					<LI text="Curiosité, investissement" />
+					<LI text="Autonomie" />
+				</ItemList>
+				<Item solo text="Méthodes agiles" />
+				<Item solo text="Intégration continue" />
+				<ItemList title="Langues">
+					<LI text="Anglais" rating="3" />
+					<LI text="Espagnol" rating="0" />
+					<LI text="Russe" rating="0" />
+					<LI text="Esperanto" rating="0" />
+				</ItemList>
+			</Box>
+			<Box title="Parcours pro" dated spaced>
+				<Item text="Manutention" date="2020" />
+				<div class="sub">Entreprises de cartonnage</div>
+				<div class="sub">Agriculture</div>
+				<div class="sub">Livraison meubles</div>
+				<Item text="Aide-dentaire" />
+				<Item text="Commis de cuisine/crêpier" date="2017" />
+				<Item text="Stage développeur full-stack @Sumwhere" date="2021" />
+			</Box>
+			<Box title="formation">
+				<Item
+					text="Titre pro. développeur Web/Web mobile JS/Java"
+					under="Simplon - Sept 2020 - avr 2021 [Bac+2]"
+				/>
+				<Item
+					text="Certification - Excellence en maîtrise qualité Web"
+					under="Opquast - Décembre 2020"
+				/>
+				<Item
+					text="L1 - Langues étrangères appliquées"
+					under="Université de Nantes - 2016/2017"
+				/>
+				<Item text="Bac. Economique et Social" under="Lycée Francois Rabelais (85200) - 2016" />
+			</Box>
+			<Box title="loisirs">
+				<Item solo text="Lecture" />
+				<Item solo text="Cinéma de genre, absurde" />
+				<Item solo text="Web" />
+				<div class="sub">Développement de webapps - projets perso</div>
+				<Item solo text="Voyages" />
+				<div class="sub">(Islande, Slovaquie, Ecosse..)</div>
+				<Item solo text="Randonnée" />
+				<Item solo text="Aventure !" />
+			</Box>
+			<div class="backToTop_wrapper">
+				<div class="backToTop" on:click={scrollToTop}>
+					<div class="label">Retour en haut</div>
+					<img src="assets/arrow-up.svg" alt="" />
+				</div>
+			</div>
+		</div>
+	{:else}
+		<div class="tab" in:fly|local={{x: 500, delay: 300}} out:fly|local={{x: 500}}>
+			<Box centered>
+				<div class="elem">
+					<div class="label">Téléphone</div>
+					<div class="value">0634394117</div>
+				</div>
+				<div class="elem">
+					<div class="label">Mail</div>
+					<div class="value">joseph.allain24@gmail.com</div>
+				</div>
+				<div class="elem">
+					<div class="label">Adresse</div>
+					<div class="value">76 route de Vertou 44200 Nantes</div>
+				</div>
+				{#if ready}
+					<Map />
+				{/if}
+			</Box>
+		</div>
+	{/if}
+</main>
 
 <style>
 	main {
@@ -121,113 +225,3 @@
 		}
 	}
 </style>
-
-<main in:fade>
-	<img src="assets/blob1.svg" alt="" class="blob1" />
-	<img src="assets/blob2.svg" alt="" class="blob2" />
-	{#if $openTab == 'infos'}
-		<div
-			class="tab"
-			in:fly|local={{ x: -500, delay: 300 }}
-			out:fly|local={{ x: -500 }}>
-			<Box title="Dev skills">
-				<ItemList title="Web">
-					<LI text="HTML5, CSS/SASS responsive" rating="5" />
-					<LI text="Javascript, Typescript" rating="5" />
-					<LI text="Svelte.js" rating="5" />
-					<LI text="Google Firebase" rating="4" />
-					<LI text="Vue.js" rating="3" />
-					<LI text="Angular" rating="2" />
-					<LI text="React" rating="1" />
-				</ItemList>
-				<ItemList title="Node">
-					<LI
-						text="Module Bundlers (Rollup, Snowpack..)"
-						rating="3" />
-					<LI text="Electron" rating="3" />
-				</ItemList>
-				<ItemList title="Java">
-					<LI text="Maven, Spring, Hibernate" rating="1" />
-					<LI text="JDK 1.8" rating="1" />
-				</ItemList>
-				<Item solo text="MySQL" />
-			</Box>
-			<Box title="Skills">
-				<ItemList title="Soft skills">
-					<LI text="Pédagogie et écoute" />
-					<LI text="Curiosité, investissement" />
-					<LI text="Autonomie" />
-				</ItemList>
-				<Item solo text="Méthodes agiles" />
-				<Item solo text="Intégration continue" />
-				<ItemList title="Langues">
-					<LI text="Anglais" rating="3" />
-					<LI text="Espagnol" rating="0" />
-					<LI text="Russe" rating="0" />
-					<LI text="Esperanto" rating="0" />
-				</ItemList>
-			</Box>
-			<Box title="Parcours pro" dated spaced>
-				<Item text="Manutention" date="2020" />
-				<div class="sub">Entreprises de cartonnage</div>
-				<div class="sub">Agriculture</div>
-				<div class="sub">Livraison meubles</div>
-				<Item text="Aide-dentaire" />
-				<Item text="Commis de cuisine/crêpier" date="2017" />
-				<Item
-					text="Stage développeur full-stack @Sumwhere"
-					date="2021" />
-			</Box>
-			<Box title="formation">
-				<Item
-					text="Titre professionel développeur Web/Web mobile"
-					under="Simplon - Sept 2020 - avr 2021 [Bac+2]" />
-				<Item
-					text="L1 - Langues étrangères appliquées"
-					under="Université de Nantes - 2016/2017" />
-				<Item
-					text="Bac. Economique et Social"
-					under="Lycée Francois Rabelais (85200) - 2016" />
-			</Box>
-			<Box title="loisirs">
-				<Item solo text="Lecture" />
-				<Item solo text="Cinéma de genre, absurde" />
-				<Item solo text="Web" />
-				<div class="sub">Développement de webapps - projets perso</div>
-				<Item solo text="Voyages" />
-				<div class="sub">(Islande, Slovaquie, Ecosse..)</div>
-				<Item solo text="Randonnée" />
-				<Item solo text="Aventure !" />
-			</Box>
-			<div class="backToTop_wrapper">
-				<div class="backToTop" on:click={scrollToTop}>
-					<div class="label">Retour en haut</div>
-					<img src="assets/arrow-up.svg" alt="" />
-				</div>
-			</div>
-		</div>
-	{:else}
-		<div
-			class="tab"
-			in:fly|local={{ x: 500, delay: 300 }}
-			out:fly|local={{ x: 500 }}>
-			<Box centered>
-				<div class="elem">
-					<div class="label">Téléphone</div>
-					<div class="value">0634394117</div>
-				</div>
-				<div class="elem">
-					<div class="label">Mail</div>
-					<div class="value">joseph.allain24@gmail.com</div>
-				</div>
-				<div class="elem">
-					<div class="label">Adresse</div>
-					<div class="value">76 route de Vertou 44200 Nantes</div>
-				</div>
-				{#if ready}
-					<Map />
-				{/if}
-			</Box>
-		</div>
-	{/if}
-</main>
